0785848088417d1d429989667e24cf08617e0024
Gourav Girish Shintre <gouravshintre02@gmail.com>
2024-10-28 14:30:35 +0530

    modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/',
+  build: {
+    rollupOptions: {
+      external: ['framer-motion'], // Add other external dependencies if needed
+    }
+  }
 })
