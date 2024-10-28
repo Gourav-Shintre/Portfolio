029931740c9745a4a3126207ab0ef29c43750408
Gourav Girish Shintre <gouravshintre02@gmail.com>
2024-10-28 14:12:42 +0530

    modified package.json

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // Adjust this based on your deployment path
-  build: {
-    rollupOptions: {
-      // Optional: Externalize specific packages if necessary
-      external: ['framer-motion'], // If you need to externalize this package
-    },
-    // Optional: Configure output directory
-    outDir: 'dist', // Ensure this matches your deployment setup
-  }
-});
+})
